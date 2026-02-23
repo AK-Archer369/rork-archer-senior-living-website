8dc8ff834de278700fc6fcc71430c1520449434f
AK-Archer369 <gattaca3669@gmail.com>
2026-02-22 23:01:14 -0500

    Updated app-build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("a4bb2733a047ea0c")
+/******/ 		__webpack_require__.h = () => ("64e86a3651734908")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4af241526f652e0e"; }
+/******/ 		__webpack_require__.h = function() { return "7e819ed815743137"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
